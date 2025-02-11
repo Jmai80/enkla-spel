10f134e718f4e9f02454ad3047d6dffb6f73e69e
Jimmy Drane <jimmydrane@MacBook-Air-som-tillhor-Jimmy.local>
2025-02-11 16:38:46 +0100

    lägger till funktioner för bla knappar i engelska2

--- andrasidan/engelska2/app.js
+++ andrasidan/engelska2/app.js
@@ -1,237 +1,4 @@
-const words = [
-    { sv: "upprörd", en: "upset" },
-    { sv: "tredje", en: "third" },
-    { sv: "att hacka", en: "chop" },
-    { sv: "slott", en: "palace" },
-    { sv: "gymnasium", en: "high school" },
-    { sv: "fegis", en: "coward" },
-    { sv: "att skära", en: "cut" },
-    { sv: "skratta", en: "laugh" },
-    { sv: "hoppa", en: "jump" },
-    { sv: "hoppas", en: "hope" },
-    { sv: "fönster", en: "window" },
-    { sv: "modig", en: "brave" },
-    { sv: "bokhylla", en: "bookshelf" },
-    { sv: "glad", en: "happy" },
-    { sv: "sjunga", en: "sing" },
-    { sv: "skugga", en: "shadow" },
-    { sv: "tyst", en: "quiet" },
-    { sv: "rita", en: "draw" },
-    { sv: "berg", en: "mountain" },
-    { sv: "snabb", en: "fast" },
-    { sv: "pekfinger", en: "index finger" },
-    { sv: "kudde", en: "pillow" },
-    { sv: "tålmodig", en: "patient" },
-    { sv: "strand", en: "beach" },
-    { sv: "arg", en: "angry" },
-    { sv: "att springa", en: "run" },
-    { sv: "moln", en: "cloud" },
-    { sv: "vänlig", en: "friendly" },
-    { sv: "skriva", en: "write" },
-    { sv: "skog", en: "forest" },
-    { sv: "svag", en: "weak" },
-    { sv: "simma", en: "swim" },
-    { sv: "bygga", en: "build" },
-    { sv: "öken", en: "desert" },
-    { sv: "snäll", en: "kind" },
-    { sv: "läsa", en: "read" },
-    { sv: "flyga", en: "fly" },
-    { sv: "tålamod", en: "patience" },
-    { sv: "förvirrad", en: "confused" },
-    { sv: "klättra", en: "climb" },
-    { sv: "aska", en: "ashes" },
-    { sv: "björk", en: "birch" },
-    { sv: "förgäves", en: "in vain" },
-    { sv: "tveksam", en: "doubtful" },
-    { sv: "utforska", en: "explore" },
-    { sv: "mysterium", en: "mystery" },
-    { sv: "beslutsam", en: "determined" },
-    { sv: "försvinna", en: "disappear" },
-    { sv: "övertygad", en: "convinced" },
-    { sv: "upptäcka", en: "discover" },
-    { sv: "förväntan", en: "expectation" },
-    { sv: "övervinna", en: "overcome" },
-    { sv: "överflöd", en: "abundance" },
-    { sv: "förbjuda", en: "forbid" },
-    { sv: "överlevnad", en: "survival" },
-    { sv: "förstärka", en: "strengthen" },
-    { sv: "överenskommelse", en: "agreement" },
-    { sv: "försvara", en: "defend" },
-    { sv: "övertygelse", en: "conviction" },
-    { sv: "ändra", en: "change" },
-    { sv: "överlägsen", en: "superior" },
-    { sv: "förstöra", en: "destroy" },
-    { sv: "överraskad", en: "surprised" },
-    { sv: "förbättra", en: "improve" },
-    { sv: "överflödig", en: "redundant" },
-    { sv: "förlåta", en: "forgive" },
-    { sv: "överdriven", en: "exaggerated" },
-    { sv: "förhindra", en: "prevent" },
-    { sv: "förstå", en: "understand" },
-    { sv: "övertygande", en: "persuasive" },
-    { sv: "förklara", en: "explain" },
-    { sv: "retas", en: "tease" },
-    { sv: "matsked", en: "tablespoon" },
-    { sv: "fågel", en: "bird" },
-    { sv: "flod", en: "river" },
-    { sv: "sten", en: "stone" },
-    { sv: "träd", en: "tree" },
-    { sv: "blomma", en: "flower" },
-    { sv: "gräs", en: "grass" },
-    { sv: "måne", en: "moon" },
-    { sv: "stjärna", en: "star" },
-    { sv: "eld", en: "fire" },
-    { sv: "vatten", en: "water" },
-    { sv: "dal", en: "valley" },
-    { sv: "sjö", en: "lake" },
-    { sv: "hav", en: "ocean" },
-    { sv: "djur", en: "animal" },
-    { sv: "katt", en: "cat" },
-    { sv: "hund", en: "dog" },
-    { sv: "fisk", en: "fish" },
-    { sv: "orm", en: "snake" },
-    { sv: "groda", en: "frog" },
-    { sv: "insekt", en: "insect" },
-    { sv: "fjäril", en: "butterfly" },
-    { sv: "bi", en: "bee" },
-    { sv: "myra", en: "ant" },
-    { sv: "spindel", en: "spider" },
-    { sv: "sköldpadda", en: "turtle" },
-    { sv: "delfin", en: "dolphin" },
-    { sv: "val", en: "whale" },
-    { sv: "haj", en: "shark" },
-    { sv: "papegoja", en: "parrot" },
-    { sv: "örn", en: "eagle" },
-    { sv: "uggla", en: "owl" },
-    { sv: "kanin", en: "rabbit" },
-    { sv: "råtta", en: "rat" },
-    { sv: "ekorre", en: "squirrel" },
-    { sv: "igelkott", en: "hedgehog" },
-    { sv: "lejon", en: "lion" },
-    { sv: "björn", en: "bear" },
-    { sv: "varg", en: "wolf" },
-    { sv: "räv", en: "fox" },
-    { sv: "älg", en: "moose" },
-    { sv: "hjort", en: "deer" },
-    { sv: "giraff", en: "giraffe" },
-    { sv: "elefant", en: "elephant" },
-    { sv: "noshörning", en: "rhinoceros" },
-    { sv: "fladdermus", en: "bat" },
-    { sv: "pingvin", en: "penguin" },
-    { sv: "schimpans", en: "chimpanzee" },
-    { sv: "orangutang", en: "orangutan" },
-    { sv: "flodhäst", en: "hippopotamus" },
-    { sv: "ödla", en: "lizard" },
-    { sv: "struts", en: "ostrich" },
-    { sv: "falk", en: "falcon" },
-    { sv: "hök", en: "hawk" },
-    { sv: "kråka", en: "crow" },
-    { sv: "sparv", en: "sparrow" },
-    { sv: "kolibri", en: "hummingbird" },
-    { sv: "säl", en: "seal" },
-    { sv: "sjölejon", en: "sea lion" },
-    { sv: "valross", en: "walrus" },
-    { sv: "bläckfisk", en: "octopus" },
-    { sv: "blåval", en: "blue whale" },
-    { sv: "manet", en: "jellyfish" },
-    { sv: "sjöstjärna", en: "starfish" },
-    { sv: "skogssnigel", en: "slug" },
-    { sv: "snäcka", en: "snail" },
-    { sv: "hummer", en: "lobster" },
-    { sv: "räka", en: "shrimp" },
-    { sv: "blåbär", en: "blueberry" },
-    { sv: "hallon", en: "raspberry" },
-    { sv: "jordgubbe", en: "strawberry" },
-    { sv: "ananas", en: "pineapple" },
-    { sv: "vindruva", en: "grape" },
-    { sv: "vattenmelon", en: "watermelon" },
-    { sv: "persika", en: "peach" },
-    { sv: "plommon", en: "plum" },
-    { sv: "aprikos", en: "apricot" },
-    { sv: "körsbär", en: "cherry" },
-    { sv: "fikon", en: "fig" },
-    { sv: "citron", en: "lemon" },
-    { sv: "tomat", en: "tomato" },
-    { sv: "gurka", en: "cucumber" },
-    { sv: "morot", en: "carrot" },
-    { sv: "potatis", en: "potato" },
-    { sv: "lök", en: "onion" },
-    { sv: "vitlök", en: "garlic" },
-    { sv: "spenat", en: "spinach" },
-    { sv: "sallad", en: "lettuce" },
-    { sv: "blomkål", en: "cauliflower" },
-    { sv: "paprika", en: "bell pepper" },
-    { sv: "aubergine", en: "eggplant" },
-    { sv: "ärtor", en: "peas" },
-    { sv: "bönor", en: "beans" },
-    { sv: "linser (mat)", en: "lentils" },
-    { sv: "kikärtor", en: "chickpeas" },
-    { sv: "grönsak", en: "vegetable" },
-    { sv: "smör", en: "butter" },
-    { sv: "kött", en: "meat" },
-    { sv: "fläskkött", en: "pork" },
-    { sv: "nötkött", en: "beef" },
-    { sv: "lamm", en: "lamb" },
-    { sv: "musslor", en: "mussels" },
-    { sv: "ostron", en: "oysters" },
-    { sv: "kräfta", en: "crayfish" },
-    { sv: "kullerbytta", en: "somersault" },
-    { sv: "krabba", en: "crab" },
-    { sv: "regnbåge", en: "rainbow" },
-    { sv: "höst", en: "autumn" },
-    { sv: "vår", en: "spring" },
-    { sv: "dröm", en: "dream" },
-    { sv: "morgon", en: "morning" },
-    { sv: "kväll", en: "evening" },
-    { sv: "skymning", en: "dusk" },
-    { sv: "evighet", en: "eternity" },
-    { sv: "ögonblick", en: "moment" },
-    { sv: "sanning", en: "truth" },
-    { sv: "lögn", en: "lie" },
-    { sv: "visdom", en: "wisdom" },
-    { sv: "fnittra", en: "giggle" },
-    { sv: "äventyr", en: "adventure" },
-    { sv: "oändlig", en: "infinite" },
-    { sv: "mystisk", en: "mysterious" },
-    { sv: "glänta", en: "glade" },
-    { sv: "blunda", en: "close your eyes" },
-    { sv: "gapa", en: "open your mouth" },
-    { sv: "diska", en: "do the dishes" },
-    { sv: "gul", en: "yellow" },
-    { sv: "lila", en: "purple" },
-    { sv: "rosa", en: "pink" },
-    { sv: "brun", en: "brown" },
-    { sv: "grå", en: "gray" },
-    { sv: "vacker", en: "beautiful" },
-    { sv: "ful", en: "ugly" },
-    { sv: "lugn", en: "calm" },
-    { sv: "mjuk", en: "soft" },
-    { sv: "hård", en: "hard" },
-    { sv: "farlig", en: "dangerous" },
-    { sv: "rolig", en: "funny" },
-    { sv: "tråkig", en: "boring" },
-    { sv: "dyster", en: "gloomy" },
-    { sv: "snöig", en: "snowy" },
-    { sv: "regnig", en: "rainy" },
-    { sv: "kärlek", en: "love" },
-    { sv: "frihet", en: "freedom" },
-    { sv: "rättvisa", en: "justice" },
-    { sv: "förälder", en: "parent" },
-    { sv: "syskon", en: "sibling" },
-    { sv: "vän", en: "friend" },
-    { sv: "fiende", en: "enemy" },
-    { sv: "elev", en: "student" },
-    { sv: "stad", en: "city" },
-    { sv: "by", en: "village" },
-    { sv: "land", en: "country" },
-    { sv: "värld", en: "world" },
-    { sv: "bergskedja", en: "mountain range" },
-    { sv: "bro", en: "bridge" },
-    { sv: "väg", en: "road" },
-    { sv: "solnedgång", en: "sunset" },
-    { sv: "soluppgång", en: "sunrise" }
-  ];
-  
+
   let currentWordIndex = 0;
   let score = 0;
   let errors = 0;
@@ -248,6 +15,9 @@ const words = [
   
   // Starta spelet
   function startGame() {
+    // Blanda frågorna så att de visas i slumpmässig ordning
+    shuffleArray(words);
+    
     currentWordIndex = 0;
     score = 0;
     errors = 0;
@@ -260,7 +30,7 @@ const words = [
     startTimer();
   }
   
-  // Använder Fisher–Yates-algoritmen för att blanda en array
+  // Fisher–Yates-algoritmen för att blanda en array
   function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -273,10 +43,10 @@ const words = [
   function loadWord() {
     const currentWord = words[currentWordIndex];
     wordDisplay.textContent = currentWord.sv;
-  
+    
     // Skapa en array med det korrekta svaret
     const options = [currentWord.en];
-  
+    
     // Lägg till slumpade, unika felalternativ
     while (options.length < 5) {
       const randomAnswer = words[Math.floor(Math.random() * words.length)].en;
@@ -284,16 +54,15 @@ const words = [
         options.push(randomAnswer);
       }
     }
-  
-    // Blanda alternativen med Fisher–Yates
+    
+    // Blanda svarsalternativen med Fisher–Yates
     shuffleArray(options);
-  
+    
     // Rensa tidigare alternativ och skapa nya knappar
     optionsContainer.innerHTML = "";
     options.forEach(option => {
       const button = document.createElement("button");
       button.textContent = option;
-      // Vid klick skickas knappen, det valda svaret och det korrekta svaret
       button.addEventListener("click", function() {
         handleAnswerClick(this, option, currentWord.en);
       });
@@ -306,7 +75,7 @@ const words = [
     // Inaktivera alla svarsknappar
     Array.from(optionsContainer.children).forEach(btn => btn.disabled = true);
     clearInterval(timer);
-  
+    
     if (selectedAnswer === correctAnswer) {
       score++;
       scoreDisplay.textContent = score;
@@ -314,7 +83,9 @@ const words = [
     } else {
       errors++;
       errorsDisplay.textContent = errors;
-      clickedButton.style.backgroundColor = "red";
+      if (clickedButton) {
+        clickedButton.style.backgroundColor = "red";
+      }
       // Markera det korrekta svaret
       Array.from(optionsContainer.children).forEach(btn => {
         if (btn.textContent === correctAnswer) {
@@ -322,7 +93,7 @@ const words = [
         }
       });
     }
-  
+    
     // Vänta en sekund innan nästa ord
     setTimeout(() => {
       if (errors >= 5) {
@@ -354,13 +125,13 @@ const words = [
     }, 1000);
   }
   
-  // Avsluta spelet och visa popup med slutresultat och en omstart-knapp
+  // Avsluta spelet och visa popup med slutresultat samt en omstart-knapp
   function endGame() {
     clearInterval(timer);
     document.querySelector(".game-container").classList.add("blur");
     popup.style.display = "flex";
     finalScoreDisplay.textContent = score;
-  
+    
     // Skapa omstartsknapp om den inte redan finns
     if (!document.getElementById("restart-button")) {
       const restartButton = document.createElement("button");
@@ -373,7 +144,6 @@ const words = [
   
   // Återställ spelet
   function restartGame() {
-    // Ta bort omstartsknappen från popupen
     const restartButton = document.getElementById("restart-button");
     if (restartButton) {
       restartButton.remove();
@@ -381,5 +151,11 @@ const words = [
     startGame();
   }
   
-  startGame();
+  // Lyssna på startknappen på startskärmen
+  document.getElementById("start-button").addEventListener("click", function() {
+    // Döljer startskärmen
+    document.getElementById("start-screen").style.display = "none";
+    // Startar spelet
+    startGame();
+  });
   
